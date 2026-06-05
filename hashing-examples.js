573ec7c992e1481bda45363721073b6954d295f2
sunnydosanjh <64918142+sunnydosanjh@users.noreply.github.com>
2026-06-05 09:03:27 -0700

    Create hashing-examples.js

---
+++ hashing-examples.js
@@ -0,0 +1,7 @@
+const crypto = require("crypto");
+
+crypto.createHash("sha256");
+crypto.createHash("sha512");
+crypto.createHash("sha3-512");
+crypto.createHash("md5");
+crypto.createHash("sha1");
